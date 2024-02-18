0521153a4f775f75b91570d842699ec1663e08d2
MyJetson <zoe.heidt@gmail.com>
2024-02-18 16:43:04 -0500

    trying to make this work

--- scripts/preTrainingScript.py
+++ scripts/preTrainingScript.py
@@ -1,16 +1,23 @@
 
-import argparse as args
+#import argparse as args
 import os
 import numpy as np
 import cv2
+import random 
 
 Path = "/home/jetson/workspace/2024VisionProject/mk1"
+DataPath = "/home/jetson/workspace/2024VisionProject/BetterData"
 
-Train = open( Path + "/Train.txt", "w")
-Validate = open(Path + "/Validate.txt", "w")
-Test  = open(Path + "/Test.txt", "w")
+#Train = open( Path + "/Train.txt", "w")
+#Validate = open(Path + "/Validate.txt", "w")
+#Test  = open(Path + "/Test.txt", "w")
 
 Train = 0.6
 Validate = 0.15
 Test = 0.25
 
+jpgList = []
+for each in os.listdir(DataPath):
+    if each.endswith(".jpg"):
+        jpgList.append(each)
+print(jpgList)
