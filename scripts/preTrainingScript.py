a52cc3c398877675ae3bbd47177e54a5057c5472
MyJetson <zoe.heidt@gmail.com>
2024-02-18 19:14:44 -0500

    the scripty boi is doing the scripty boi stuff (mostly)

--- scripts/preTrainingScript.py
+++ scripts/preTrainingScript.py
@@ -8,16 +8,44 @@ import random
 Path = "/home/jetson/workspace/2024VisionProject/mk1"
 DataPath = "/home/jetson/workspace/2024VisionProject/BetterData"
 
-#Train = open( Path + "/Train.txt", "w")
-#Validate = open(Path + "/Validate.txt", "w")
-#Test  = open(Path + "/Test.txt", "w")
-
-Train = 0.6
-Validate = 0.15
-Test = 0.25
+train = 0.6
+validate = .375 #this is 15% of the whole but wee substract 60% adn tehn take teh next part so its 3/8 of the remaining
+test = 0.25
 
 jpgList = []
 for each in os.listdir(DataPath):
     if each.endswith(".jpg"):
         jpgList.append(each)
-print(jpgList)
+
+TrainData = random.sample(jpgList, k=int(len(jpgList)*train))
+Train = open( Path + "/Train.txt", "w")
+for each in TrainData:
+    imgPath = os.path.join(DataPath, each)
+    Train.write(imgPath + "\n")
+Train.close()
+    
+jpgList = [x for x in jpgList if x not in TrainData]
+#above line subtracts every element of TrainData from jpgList
+
+ValidateData = random.sample(jpgList, k = int(len(jpgList)*validate))
+Validate = open(Path + "/Validate.txt", "w")
+for each in ValidateData:
+    imgPath = os.path.join(DataPath, each)
+    Validate.write(imgPath + "\n")
+Validate.close()
+    
+TestData = [x for x in jpgList if x not in ValidateData]
+Test  = open(Path + "/Test.txt", "w")
+for each in TestData:
+    imgPath = os.path.join(DataPath, each)
+    Test.write(imgPath + "\n")
+Test.close()
+
+mk1 = open(Path + "/mk1.data", "w")
+mk1.write("train = " + Path + "/Train.txt" + "\n")
+mk1.write("validate = " + Path + "/Validate.txt" + "\n")
+mk1.write("test = " + Path + "/Test.txt" + "\n")
+mk1.write("backup = " + Path + "/backup")
+mk1.close
+
+print("done")
