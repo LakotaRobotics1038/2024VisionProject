ac9f865846dbca7a414840986c888312aa524db5
MyJetson <zoe.heidt@gmail.com>
2024-02-24 12:52:39 -0500

    argparse might work i think

--- scripts/preTrainingScript.py
+++ scripts/preTrainingScript.py
@@ -1,12 +1,28 @@
 
-#import argparse as args
+import argparse as args
+from pathlib import Path 
 import os
 import numpy as np
 import cv2
 import random 
 
-Path = "/home/jetson/workspace/2024VisionProject/mk1"
-DataPath = "/home/jetson/workspace/2024VisionProject/BetterData"
+#Path = "/home/jetson/workspace/2024VisionProject/mk1"
+#DataPath = "/home/jetson/workspace/2024VisionProject/BetterData"
+def __init__:
+    parser = argparse.AgrumentParser()
+    parser.addArgument(
+        "--path-dir",
+        type = Path,
+        default= Path(__file__).absolute().parent / "path",
+        help= os.path.join("home", "jetson","workspace","2024VisionProject","mk1"))
+
+    parser = argparse.ArgumentParser()
+    parser.addArgument(
+        "--data-dir",
+        type = Path,
+        default= Path(__file__).absolute().parent / "data",
+        help= os.path.join("home", "jetson","workspace","2024VisionProject","mk1"))
+        
 
 train = 0.6
 validate = .375 #this is 15% of the whole but wee substract 60% adn tehn take teh next part so its 3/8 of the remaining
@@ -18,7 +34,7 @@ for each in os.listdir(DataPath):
         jpgList.append(each)
 
 TrainData = random.sample(jpgList, k=int(len(jpgList)*train))
-Train = open( Path + "/Train.txt", "w")
+Train = open(os.path.join(Path,"Train.txt"), "w")
 for each in TrainData:
     imgPath = os.path.join(DataPath, each)
     Train.write(imgPath + "\n")
@@ -28,14 +44,14 @@ jpgList = [x for x in jpgList if x not in TrainData]
 #above line subtracts every element of TrainData from jpgList
 
 ValidateData = random.sample(jpgList, k = int(len(jpgList)*validate))
-Validate = open(Path + "/Validate.txt", "w")
+Validate = open(os.path.join(Path,"Validate.txt"), "w")
 for each in ValidateData:
     imgPath = os.path.join(DataPath, each)
     Validate.write(imgPath + "\n")
 Validate.close()
     
 TestData = [x for x in jpgList if x not in ValidateData]
-Test  = open(Path + "/Test.txt", "w")
+Test  = open(os.path.join(Path,"Test.txt"), "w")
 for each in TestData:
     imgPath = os.path.join(DataPath, each)
     Test.write(imgPath + "\n")
