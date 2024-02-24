a0ead1b58ef6697f6931105d02180c05da8d175e
MyJetson <zoe.heidt@gmail.com>
2024-02-24 14:05:27 -0500

    it work, i might cry but preTraing works

--- scripts/preTrainingScript.py
+++ scripts/preTrainingScript.py
@@ -1,42 +1,44 @@
 
-import argparse as args
+import argparse
 from pathlib import Path 
 import os
 import numpy as np
 import cv2
 import random 
 
-#Path = "/home/jetson/workspace/2024VisionProject/mk1"
-#DataPath = "/home/jetson/workspace/2024VisionProject/BetterData"
-def __init__:
-    parser = argparse.AgrumentParser()
-    parser.addArgument(
-        "--path-dir",
-        type = Path,
-        default= Path(__file__).absolute().parent / "path",
-        help= os.path.join("home", "jetson","workspace","2024VisionProject","mk1"))
+# os.path.join(os.path.expanduser('~'), '
 
-    parser = argparse.ArgumentParser()
-    parser.addArgument(
-        "--data-dir",
-        type = Path,
-        default= Path(__file__).absolute().parent / "data",
-        help= os.path.join("home", "jetson","workspace","2024VisionProject","mk1"))
-        
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--path-dir",
+    dest = "path",
+    type = Path,
+    default= os.path.join(os.path.expanduser('~'), 'workspace', '2024VisionProject', 'mk1')
+    )
+
+
+parser.add_argument(
+    "--data-dir",
+    dest = "data",
+    type = Path,
+    default= os.path.join(os.path.expanduser('~'), 'workspace', '2024VisionProject','BetterData')
+    )
+
+args = parser.parse_args()
 
 train = 0.6
 validate = .375 #this is 15% of the whole but wee substract 60% adn tehn take teh next part so its 3/8 of the remaining
 test = 0.25
 
 jpgList = []
-for each in os.listdir(DataPath):
+for each in os.listdir(args.data):
     if each.endswith(".jpg"):
         jpgList.append(each)
 
 TrainData = random.sample(jpgList, k=int(len(jpgList)*train))
-Train = open(os.path.join(Path,"Train.txt"), "w")
+Train = open(os.path.join(args.path,"Train.txt"), "w")
 for each in TrainData:
-    imgPath = os.path.join(DataPath, each)
+    imgPath = os.path.join(args.data, each)
     Train.write(imgPath + "\n")
 Train.close()
     
@@ -44,24 +46,24 @@ jpgList = [x for x in jpgList if x not in TrainData]
 #above line subtracts every element of TrainData from jpgList
 
 ValidateData = random.sample(jpgList, k = int(len(jpgList)*validate))
-Validate = open(os.path.join(Path,"Validate.txt"), "w")
+Validate = open(os.path.join(args.path,"Validate.txt"), "w")
 for each in ValidateData:
-    imgPath = os.path.join(DataPath, each)
+    imgPath = os.path.join(args.data, each)
     Validate.write(imgPath + "\n")
 Validate.close()
     
 TestData = [x for x in jpgList if x not in ValidateData]
-Test  = open(os.path.join(Path,"Test.txt"), "w")
+Test  = open(os.path.join(args.path,"Test.txt"), "w")
 for each in TestData:
-    imgPath = os.path.join(DataPath, each)
+    imgPath = os.path.join(args.data, each)
     Test.write(imgPath + "\n")
 Test.close()
 
-mk1 = open(Path + "/mk1.data", "w")
-mk1.write("train = " + Path + "/Train.txt" + "\n")
-mk1.write("validate = " + Path + "/Validate.txt" + "\n")
-mk1.write("test = " + Path + "/Test.txt" + "\n")
-mk1.write("backup = " + Path + "/backup")
+mk1 = open(os.path.join(args.path, "mk1.data"), "w")
+mk1.write("train = " + os.path.join(args.path,"Train.txt") + "\n")
+mk1.write("validate = " + os.path.join(args.path, "Validate.txt") + "\n")
+mk1.write("test = " + os.path.join(args.path, "Test.txt") + "\n")
+mk1.write("backup = " + os.path.join(args.path, "backup"))
 mk1.close()
 
 print("done")
