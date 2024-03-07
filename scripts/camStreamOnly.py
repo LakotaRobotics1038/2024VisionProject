5550685c41d87a1000e571c3232294333e603bde
1038 Pit <1038Pit@1038-PIT-L1.ad.lakotarobotics.org>
2024-03-07 16:08:20 -0500

    fixing camera stream only

--- scripts/camStreamOnly.py
+++ scripts/camStreamOnly.py
@@ -1,33 +1,33 @@
 from flask import Flask, Response
 import cv2
-from networktables import NetworkTables
+# from networktables import NetworkTables
 from datetime import datetime
 
 # declares cameras and sets our server address
 app = Flask(__name__)
 cam0 = cv2.VideoCapture(0)
-cam1 = cv2.VideoCapture(1)
+# cam1 = cv2.VideoCapture(1)
 serverAddr = '10.10.38.2'
 
-NetworkTables.initialize(server=serverAddr)
+# NetworkTables.initialize(server=serverAddr)
 # gets custom table
-tables = NetworkTables.getTable('Vision')
+# tables = NetworkTables.getTable('Vision')
 
 def get_image():
-    ret, img = cam1.read()
     ret, img = cam0.read()
+    # ret, img = cam1.read()
     # default port for network tables = 1735
 
     while True:
-        shouldStream0 = tables.getboolean('shouldStream0', True)
+        # shouldStream0 = tables.getboolean('shouldStream0', True)
 
-        if (shouldStream0):
-            ret, img = cam0.read()
-        else:
-            ret, img = cam1.read()
+        # if (shouldStream0):
+        ret, img = cam0.read()
+        # else:
+        #     ret, img = cam1.read()
 
-        img = cv2.resize(img.resize(160, 120))
-        _, frame = cv2.imcode('jpg', img)
+        img = cv2.resize(img, (160, 120))
+        _, frame = cv2.imencode('.jpg', img)
         yield b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + frame.tobytes() + b'\r\n'
 
 @app.route('/stream')
