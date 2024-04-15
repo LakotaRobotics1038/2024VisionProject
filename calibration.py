4c941948e91d30ef212301d8a7cc7d126c8bdf6e
Murat65536 <bmuratb502@gmail.com>
2024-04-15 19:54:18 -0400

    Add image display.

--- calibration.py
+++ calibration.py
@@ -1,5 +1,5 @@
+import cv2
 from numpy import zeros, mgrid, float32
-from cv2 import TERM_CRITERIA_EPS, TERM_CRITERIA_MAX_ITER, imread, cvtColor, COLOR_BGR2GRAY, findChessboardCorners, cornerSubPix, calibrateCamera
 from glob import glob
 from typing import Tuple, Any
 
@@ -19,25 +19,30 @@ imgpoints: Any = []  # 2d points in image plane.
 # Read images and find chessboard corners
 images = glob(r"calibration_images/*.jpeg")  # Change the path to your calibration images
 
-criteria = (TERM_CRITERIA_EPS + TERM_CRITERIA_MAX_ITER, 30, 0.001)
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 for fname in images:
     print(fname)
-    img = imread(fname)
-    gray = cvtColor(img, COLOR_BGR2GRAY)
+    img = cv2.imread(fname)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     # Find the chessboard corners
-    ret, corners = findChessboardCorners(gray, GRID_SIZE, None)
+    ret, corners = cv2.findChessboardCorners(gray, GRID_SIZE, None)
 
     # If found, add object points, image points (after refining them)
     if ret:
         objpoints.append(OBJP)
 
-        corners2 = cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+        corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
         imgpoints.append(corners2)
+        cv2.drawChessboardCorners(img, (8,8), corners2, ret)
+        while True:
+            cv2.imshow('img', img)
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
 
 # Calibrate camera
-_, mtx, _, _, _ = calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+_, mtx, _, _, _ = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
 
 # Extract focal length and principal point from the camera matrix
 
